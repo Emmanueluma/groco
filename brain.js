84254411889b580df726a6a22e705a4a3ca84f2e
Emmanuel Uma <umaemmanuel62@gmail.com>
2022-10-13 21:07:35 +0100

    7th (responsiveness)

--- brain.js
+++ brain.js
@@ -1,37 +1,48 @@
-const icon1 = document.getElementById('icon1');
-const icon2 = document.getElementById('icon2');
-const icon3 = document.getElementById('icon3');
-const search = document.getElementById('search');
-const list = document.getElementById('list');
-const login = document.getElementById('login');
+(function () {
+    const icon0 = document.getElementById('icon0')
+    const icon1 = document.getElementById('icon1');
+    const icon2 = document.getElementById('icon2');
+    const icon3 = document.getElementById('icon3');
+    const search = document.getElementById('search');
+    const list = document.getElementById('list');
+    const login = document.getElementById('login');
+    const menu = document.getElementById('menu')
 
-icon1.onclick = () => {
-    if (search.style.display == "flex") {
-        search.style.display = "none"
+    icon0.onclick = () => {
+        if (menu.style.display == "flex") {
+            menu.style.display = "none"
+        }
+        else {
+            menu.style.display = "flex"
+        }
     }
-    else{
-        search.style.display = "flex"
+    icon1.onclick = () => {
+        if (search.style.display == "flex") {
+            search.style.display = "none"
+        }
+        else{
+            search.style.display = "flex"
+        }
     }
-}
-icon2.onclick = () => {
-    if (list.style.display == "flex") {
-        list.style.display = "none"
-        
+    icon2.onclick = () => {
+        if (list.style.display == "flex") {
+            list.style.display = "none"
+        }
+        else{
+            list.style.display = "flex"   
+        }
     }
-    else{
-        list.style.display = "flex"
-        
+    icon3.onclick = () => {
+        if (login.style.display == "flex") {
+            login.style.cssText = "display: none left: 100vw "
+        }
+        else{
+            login.style.display = "flex"
+            login.style.left = "70vw"
+        }
     }
-}
-icon3.onclick = () => {
-    if (login.style.display == "flex") {
-        login.style.cssText = "display: none left: 100vw "
-    }
-    else{
-        login.style.display = "flex"
-        login.style.left = "70vw"
-    }
-}
+}) ()
+
 
 
 
